304bd2ef930187b1296d07450cc24957170e4ebe
vanita22 <inesvanessapcm@gmail.com>
2017-05-05 11:40:44 -0300

    actualizando de nuevo

--- objetos.js
+++ objetos.js
@@ -1,3 +1,5 @@
+var pokemon = [];
+
 function Pokemon(nombre, color, vida, poderDeAtaque, nivelDeAmistad){
 
 	this.nombre = nombre;
@@ -21,21 +23,28 @@ function Pokemon(nombre, color, vida, poderDeAtaque, nivelDeAmistad){
 
 var Pikachu = new Pokemon("Pikachu", "amarillo", 100, 200, 300);
 var Charmander = new Pokemon("Charmander", "rojo", 20, 100, 300);
-var nomPoke=[];
+
 
 function Pelear(){
-	
-	printHTML(nomPoke)
-	Pokemon.forEach(function(elemento){ 
-	imprimir.innerHTML = elemento.nombre + " atacó a " + elemento.nombre + " y tiene una vida de " + elemento.Pikachu.atacar(Charmander)
-	});
+
+	alert("auxilio");
+	var pok1 = document.getElementById("pokemon1").value;
+	var pok2 = document.getElementById("pokemon2").value;
+	var pokemon1 = pokemon.filter(function(el){
+		return el.nombre == pok1;
+	})[0];
+	var pokemon2 = pokemon.filter(function(el){
+		return el.nombre == pok2;
+	})[0];
+
+	pokemon1.atacar(pokemon2);
+
+	var res = document.getElementById("resultados");
+	res.innerHTML = Pokemon1.nombre + "atacó a" + pokemon2.nombre + " y " + pokemon2.nombre + " tiene una vida de " + pokemon2.vida;
 }
 
-function printHTML (html){
-  imprimir.innerHTML = '';
-  imprimir.innerHTML = html;
-}
 
 Pikachu.atacar(Charmander);
 console.log(Charmander.vida);
 Charmander.mostrarPokemon();
+
